d0538a1c8d5720d099b1af62851461f0652b9286
Ray Wang <ray.hackmylife@gmail.com>
2020-08-05 19:58:30 +0800

    use lsp

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -4,24 +4,9 @@
 
 ;; See also init-go.el
 
-(when (maybe-require-package 'go-mode)
-  (require-package 'company-go)
-  (require-package 'company)
-  (require-package 'go-eldoc)
-  (require-package 'go-direx)
-  (setq company-tooltip-limit 20)
-  (setq company-idle-delay .1)
-  (setq company-echo-delay 0)
-  (setq company-begin-commands '(self-insert-command)))
-
-;; lsp-mode is not completely compatible to go-mode
-;; (add-hook 'go-mode-hook 'lsp-deferred)
-(add-hook 'go-mode-hook (lambda ()
-                          (go-eldoc-setup)
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (define-key go-mode-map (kbd "M-.") 'godef-jump)
-                          (define-key go-mode-map (kbd "C-c C-l") 'go-direx-pop-to-buffer)
-                          (company-mode)))
+(when (and (maybe-require-package 'go-mode)
+           (maybe-require-package 'lsp-mode))
+  (add-hook 'go-mode-hook #'lsp))
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize)
